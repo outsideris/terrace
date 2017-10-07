e46880699b26eb7fefdbd10f96d61987858ab6a4
Outsider <outsideris@gmail.com>
2017-10-07 21:54:17 +0900

    update description in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='terrace',
+    url='https://github.com/outsideris/terrace',
+    license='Mozilla Public License 2.0',
+    author='Outsider',
+    author_email='outsideris@gmail.com',
+    description='Web management UI for Terraform',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
